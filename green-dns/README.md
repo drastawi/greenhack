5e32b4146e485ffe5342e2134e2e4b3b748c0776
Dominik Rastawicki <drastawi@gmail.com>
2022-11-03 00:23:25 +0800

    Update constellix results (#3)

--- green-dns/README.md
+++ green-dns/README.md
@@ -1,3 +1,8 @@
 # greenhack
 
+```shell
+nvm use
+npm install
 cp .env_example .env
+```
+And replace the secret values with the ones that can be found here: https://manage.constellix.com/#/user
--- green-dns/carbon-dns.js
+++ green-dns/carbon-dns.js
@@ -1,13 +1,40 @@
-
 const fs = require('fs')
 const path = require('path')
+const baseRequest = require('./update_request')
+const request = require('request')
+const CryptoJS = require("crypto-js")
+
 const EastString = fs.readFileSync(path.join(__dirname, '930-data-export.json')).toString()
 const WestString = fs.readFileSync(path.join(__dirname, '930-data-export.json')).toString()
 const EastData = JSON.parse(EastString)
 const WestData = JSON.parse(WestString)
-print(EastData)
-print(WestData)
+console.log(EastData)
+console.log(WestData)
 
-//TODO update handicap
+const eastHandicap = 0
+const westHandicap = 50
 
-//Update https://api-docs.constellix.com/#3688e62b-0fb8-4093-9df4-0608a4be06ba
+const apiKey = "218f41a1-0550-4aed-b93f-0192f980069e";
+const secretKey = "e6bef67a-a8ad-42d5-b138-5f12aa9ef47d";
+function epochTime() {
+    return new Date().getTime() + '';
+}
+const time = epochTime();
+const hmac = CryptoJS.HmacSHA1( time, secretKey ).toString( CryptoJS.enc.Base64 );
+const token = apiKey + ":" + hmac + ":" + time;
+baseRequest.values[0].handicap = westHandicap
+baseRequest.values[1].handicap = eastHandicap
+request('https://api.dns.constellix.com/v1/pools/CNAME/5745',{
+        body: JSON.stringify(baseRequest),
+        method: 'PUT',
+        headers: {
+            'Content-Type' : 'application/json',
+            'x-cns-security-token' : token
+        },
+    },
+    function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            console.log(body);
+        }
+    }
+)
