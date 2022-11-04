26c08daa7635cadeaea4c36fdecd0e73d4e4211c
Dominik Rastawicki <drastawi@gmail.com>
2022-11-05 03:18:23 +0800

    True data (#4)

--- green-dns/carbon-dns.js
+++ green-dns/carbon-dns.js
@@ -4,24 +4,51 @@ const baseRequest = require('./update_request')
 const request = require('request')
 const CryptoJS = require("crypto-js")
 
-const EastString = fs.readFileSync(path.join(__dirname, '930-data-export.json')).toString()
-const WestString = fs.readFileSync(path.join(__dirname, '930-data-export.json')).toString()
-const EastData = JSON.parse(EastString)
-const WestData = JSON.parse(WestString)
-console.log(EastData)
-console.log(WestData)
+require('dotenv').config()
+const GREEN_FACTOR = {
+    VERY_GREEN: 30,
+    MODERATE: 20,
+    LATENCY_FOCUSED: 10
+}
 
-const eastHandicap = 0
-const westHandicap = 50
+// The data is meant to be loaded from CarbonAware SDK, but for the hackathon's purpose we load it as a file directly.
+const eastString = fs.readFileSync(path.join(__dirname, 'us-east-1-data.json')).toString()
+const westString = fs.readFileSync(path.join(__dirname, 'us-west-1-data.json')).toString()
+const eastData = JSON.parse(eastString)
+const westData = JSON.parse(westString)
 
-const apiKey = "218f41a1-0550-4aed-b93f-0192f980069e";
-const secretKey = "e6bef67a-a8ad-42d5-b138-5f12aa9ef47d";
+const timeEast = process.argv[2]
+const timeWest = process.argv[3]
+const westSolar = westData.series.find(o => o.name === "Solar").data.find(o => {
+    return o['Timestamp (Hour Ending)'] === timeWest
+})
+const eastSolar = eastData.series.find(o => o.name === "Solar").find(o => o['Timestamp (Hour Ending)'] === timeEast)
+const westGas = westData.series.find(o => o.name === "Natural Gas").find(o => o['Timestamp (Hour Ending)'] === timeWest)
+const eastGas = eastData.series.find(o => o.name === "Natural Gas").find(o => o['Timestamp (Hour Ending)'] === timeEast)
+
+console.log(westSolar)
+console.log(eastSolar)
+console.log(westGas)
+console.log(eastGas)
+
+// night
+const eastIntensity = 0.3
+const westIntensity = 0.9
+// day
+// const eastIntensity = 0.2
+// const eastIntensity = 0.3
+
+const eastNet = (eastIntensity-westIntensity)
+
+const eastHandicap = eastNet > 0 ? eastNet * GREEN_FACTOR : 0
+const westHandicap = eastNet < 0 ? -1 * eastNet * GREEN_FACTOR : 0
+
 function epochTime() {
     return new Date().getTime() + '';
 }
 const time = epochTime();
-const hmac = CryptoJS.HmacSHA1( time, secretKey ).toString( CryptoJS.enc.Base64 );
-const token = apiKey + ":" + hmac + ":" + time;
+const hmac = CryptoJS.HmacSHA1( time, process.env.CONSTELLIX_SECRET ).toString( CryptoJS.enc.Base64 );
+const token = process.env.CONSTELLIX_KEY + ":" + hmac + ":" + time;
 baseRequest.values[0].handicap = westHandicap
 baseRequest.values[1].handicap = eastHandicap
 request('https://api.dns.constellix.com/v1/pools/CNAME/5745',{
